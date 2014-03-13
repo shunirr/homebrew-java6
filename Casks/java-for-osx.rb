0718be65e4d081d519edec0540466942be414ce7
shunirr <m@s5r.jp>
2014-03-13 15:04:27 +0900

    add checksum, version

--- Casks/java-for-osx.rb
+++ Casks/java-for-osx.rb
@@ -1,5 +1,7 @@
 class JavaForOsx < Cask
   url 'http://support.apple.com/downloads/DL1572/ja_JP/JavaForOSX2013-05.dmg'
   homepage 'http://support.apple.com/kb/DL1572'
+  sha256 '81e1155e44b2c606db78487ca1a02e31dbb3cfbf7e0581a4de3ded9e635a704e'
+  version '2013-05'
   install 'JavaForOSX.pkg'
 end
