e0d048b7104f2c73ad9498befc3b685f78ff2953
shunirr <m@s5r.jp>
2014-03-13 15:02:08 +0900

    rename to java-for-osx

--- Casks/java-for-osx.rb
+++ Casks/java-for-osx.rb
@@ -1,4 +1,4 @@
-class Java6 < Cask
+class JavaForOsx < Cask
   url 'http://support.apple.com/downloads/DL1572/ja_JP/JavaForOSX2013-05.dmg'
   homepage 'http://support.apple.com/kb/DL1572'
   install 'JavaForOSX.pkg'
