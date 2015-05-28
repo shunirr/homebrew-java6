13762d07474a70a06ba6580e1d8174ac90be86a5
shunirr <m@s5r.jp>
2015-05-28 16:10:45 +0900

    update cask style

--- Casks/java-for-osx.rb
+++ Casks/java-for-osx.rb
@@ -1,4 +1,4 @@
-class JavaForOsx < Cask
+cask :v1 => 'java-for-osx' do
   url 'https://support.apple.com/downloads/DL1572/ja_JP/JavaForOSX2014-001.dmg'
   homepage 'http://support.apple.com/kb/DL1572'
   sha256 '97bc9b3c47af1f303710c8b15f2bcaedd6b40963c711a18da8eac1e49690a8a0'
